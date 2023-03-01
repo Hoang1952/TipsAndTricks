50d3342da3d12cb1c0ff2defbe6b7b736d2cec4a
Hoang1952 <85470528+Hoang1952@users.noreply.github.com>
2023-03-01 11:16:15 +0700

    Add DB

---
+++ TatBlog.Services/Extensions/PagedListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
+using Microsoft.EntityFrameworkCore;
+using TatBlog.Core.Collections;
+using TatBlog.Core.Contracts;
+
+namespace TatBlog.Services.Extensions
+{
+    //public static class PagedListExtensions
+    //{
+    //    public static string GetOrderExpression(
+    //        this IPagingParams pagingParams,
+    //        string defaultColumn = "Id"))
+    //}
+}
