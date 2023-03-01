cba4986697e30dec6e603bb568c9e54d2d9f8da1
Hoang1952 <85470528+Hoang1952@users.noreply.github.com>
2023-03-01 08:34:04 +0700

    Enter sample data

---
+++ TatBlog.Data/Seeders/IDataSeeder.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+
+namespace TatBlog.Data.Seeders
+{
+    public interface IDataSeeder
+    {
+        void Initialize();
+    }
+}
+
+public class DataSeeder : IDataSeeder
+{
+
+    private readonly BlogDbContext _dbContext;
+
+    public DataSeeder(BlogDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Initialize()
+    {
+        _dbContext.Database.EnsureCreated();
+
+        if (_dbContext.Posts.Any()) return;
+
+        var authors = AddAuthors();
+        var caregories = AddCategories();
+        var tags = AddTags();
+        var posts = AddPosts(authors, caregories, tags);
+    }
+
+
+    private IList<Author> AddAuthors()
+    {
+        var authors = new List<Author>()
+        {
+            new()
+            {
+                FullName = "Jason Mouth",
+                UrlSlug = "jason-mouth",
+                Email = "json@gmail.com",
+                JoinedDate = new DateTime(2022, 10, 21)
+            },
+            new()
+            {
+                FullName = "Jessica Wonder",
+                UrlSlug = "jessica-wonder",
+                Email = "jessica665@motip.com",
+                JoinedDate = new DateTime(2020, 4, 19)
+            }
+        };
+
+        _dbContext.Authors.AddRange(authors);
+        _dbContext.SaveChanges();
+
+        return authors;
+    }
+
+    private IList<Category> AddCategories()
+    {
+        var categories = new List<Category>()
+        {
+            new() {Name = ".NET Core", Description = "Is a multi-purpose development platform", UrlSlug = "net core"},
+            new() {Name = "Architeture", Description = "The art or science of building", UrlSlug = "architeture"},
+            new() {Name = "Messaging", Description = "The process of sending someone", UrlSlug = "messaging"},
+            new() {Name = "OOP", Description = "Is a programming method", UrlSlug = "object-oriented-program"},
+            new() {Name = "Design Pattern", Description = "Is a technique in object-oriented programming", UrlSlug = "design pattern"},
+        };
+
+        _dbContext.AddRange(categories);
+        _dbContext.SaveChanges();
+
+        return categories;
+    }
+
+    private IList<Tag> AddTags()
+    {
+        var tags = new List<Tag>()
+        {
+            new() {Name = "Google", Description = "Google is an internet search engine", UrlSlug = "google"},
+            new() {Name = "ASP.NET MVC", Description = "Framework is a lightweight", UrlSlug = "asp net mvc"},
+            new() {Name = "Razor Page", Description = "This is the first tutorial of a series", UrlSlug = "razor page"},
+            new() {Name = "Blazor", Description = "Blazor is a free and open-source web framework", UrlSlug = "blazor"},
+            new() {Name = "Deep Learning", Description = "Deep learning is a subset of machine learning", UrlSlug = "deep learning"},
+            new() {Name = "Neural Network", Description = "A neural network is a method in artificial intelligence", UrlSlug = "neural network"},
+        };
+        _dbContext.AddRange(tags);
+        _dbContext.SaveChanges();
+
+        return tags;
+    }
+
+    private IList<Post> AddPosts(
+    IList<Author> authors,
+    IList<Category> categories,
+    IList<Tag> tags)
+    {
+        var posts = new List<Post>()
+        {
+            new()
+            {
+                Title = "ASP.NET Core Dianostic Scenarios",
+                ShortDescription = "David and friends has a great repository filled",
+                Description = "Here's a few great DON'T and DO examples, but be sure to Star the repo and check it out for yourself!",
+                Meta = "David and friends has a great repository filled with examples",
+                UrlSlug = "aspnet-core-diagnostic-scenarios",
+                Published= true,
+                PostedDate= new DateTime(2021, 9, 30, 10, 20, 0),
+                ModifiedDate = null,
+                ViewCount = 10,
+                Author = authors[0],
+                Category= categories[0],
+                Tags= new List<Tag>()
+                {
+                    tags[0]
+                }
+            }
+        };
+
+        _dbContext.AddRange(posts);
+        _dbContext.SaveChanges();
+
+        return posts;
+    }
+}
--- TatBlog.WinApp/Program.cs
+++ TatBlog.WinApp/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TatBlog.Core.Entities;
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+
+//Tao doi tuong  dbContext de quan ly phien lam viec
+// voi CSDL va trang thai cua cac doi tuong
+var context = new BlogDbContext();
+
+//Tao doi tuong khoi tao du lieu mau
+var seeder = new DataSeeder(context);
+
+//Goi ham Initialize de nhap du lieu mau
+seeder.Initialize();
+
+//Doc danh sach tac gia tu co so du lieu
+var authors = context.Authors.ToList();
+
+//Lay kem ten tac gia va chuyen muc
+var posts = context.Posts
+    .Where(p => p.Published)
+    .OrderBy(p => p.Title)
+    .Select(p => new
+    {
+        Id = p.Id,
+        Title = p.Title,
+        ViewCount  = p.ViewCount,
+        PostedDate = p.PostedDate,
+        Author= p.Author.FullName,
+        Category= p.Category.Name,
+    })
+    .ToList();
+
+//Xuat danh sach tac gia ra man hinh
+Console.WriteLine("{0, -4}{1,-30}{2,-30}{3,12}",
+    "ID", "FullName", "Email", "Joined Date");
+
+foreach (var author in authors)
+{
+    Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}",
+        author.Id, author.FullName, author.Email, author.JoinedDate);
+}
+
+//Xuat danh sach bai viet ra man hinh
+foreach(var post in posts)
+{
+    Console.WriteLine("ID         :{0}", post.Id);
+    Console.WriteLine("Title      :{0}", post.Title);
+    Console.WriteLine("View       :{0}", post.ViewCount);
+    Console.WriteLine("Date       :{0:MM/dd/yyyy}", post.PostedDate);
+    Console.WriteLine("Author     :{0}", post.Author);
+    Console.WriteLine("Category   :{0}", post.Category);
+    Console.WriteLine("".PadRight(80, '-'));
+}
